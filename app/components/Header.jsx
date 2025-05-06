473e04e9ed29dda3fdf5de229cc265f9e06bee54
Tivadar-Ekpo <ekpotivadar@gmail.com>
2025-05-06 15:00:14 +0100

    Uploaded resume

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                 Contact Me
                 <Image src={ assets.right_arrow_white} alt='' className='w-4'/>
             </a>
-            <a href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 
+            <a href='/Tivadar-Ekpo-Resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 
             bg-white dark:text-black'>
                 My Resume
                 <Image src={assets.download_icon} alt='' className='w-4'/>
